0365a11e057fb6863a539f429cb51af912cecfe6
Sandra P <prochorskaite@outlook.com>
2023-03-15 13:39:00 +0200

    Removed unnecessary instanceOf in mandatoryCustomerValuesValidator

--- src/main/java/com/academy/service/MandatoryCustomerValuesValidator.java
+++ src/main/java/com/academy/service/MandatoryCustomerValuesValidator.java
@@ -3,24 +3,18 @@ package com.academy.service;
 import com.academy.entity.Customer;
 import com.academy.exception.ValidationException;
 
-import javax.management.MalformedObjectNameException;
-
-public class MandatoryCustomerValuesValidator extends Validator{
+public class MandatoryCustomerValuesValidator extends Validator {
     @Override
     public void validate(Object customer) {
 
-        if(customer instanceof Customer){
-            if (((Customer) customer).getFirstName().isBlank()) {
-                throw new ValidationException("First name is missing!");
-            }
-            if(((Customer) customer).getLastName().isBlank()) {
-                throw new ValidationException("Last name is missing!");
-            }
-            if(((Customer) customer).getPersonalNumber().isBlank()) {
-                throw new ValidationException("Personal code is missing!");
-            }
+        if (((Customer) customer).getFirstName().isBlank()) {
+            throw new ValidationException("First name is missing!");
         }
-
-
+        if (((Customer) customer).getLastName().isBlank()) {
+            throw new ValidationException("Last name is missing!");
+        }
+        if (((Customer) customer).getPersonalNumber().isBlank()) {
+            throw new ValidationException("Personal code is missing!");
+        }
     }
 }
